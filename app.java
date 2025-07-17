e77ed769c20d361cbda46b74ce0d3df87eeff0ae
devo51 <vwdevops51@gmail.com>
2025-07-17 16:14:29 +0530

    Update app.java with few more lines

--- app.java
+++ app.java
@@ -1,2 +1,4 @@
 Hi all
 This is a sample code for practice purpose.
+Koduri Srisaila Sri Rajamouli was born on 10 October 1973 in a Telugu family[11] to V. Vijayendra Prasad and Raja Nandini.
+Rajamouli was passionate about storytelling right from his second standard.
